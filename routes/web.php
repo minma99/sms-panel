bdc530fe0417776aa4c48e346ce4eb6edebe7df3
minma99 <139114060+minma99@users.noreply.github.com>
2026-06-18 21:46:42 +0200

    feat(routes): add trainee resource routes to superadmin panel

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SuperAdmin\CourseController;
 use App\Http\Controllers\SuperAdmin\PaymentController;
+use App\Http\Controllers\SuperAdmin\TraineeController;
 
 Route::prefix('superadmin')->group(function () {
 
     Route::resource('courses', CourseController::class);
 
     Route::resource('payments', PaymentController::class)
-        ->only(['index','create','store','destroy']);
+        ->only(['index', 'create', 'store', 'destroy']);
+
+    Route::resource('trainees', TraineeController::class);
 
 });
